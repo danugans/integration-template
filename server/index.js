950e64978f88fba8ae8f928278d2a4f73544cab4
danugans <mas.danu29@gmail.com>
2022-02-15 09:33:42 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 // import dotenv and call config function to load environment
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
-const cors = require('cors')
+const cors = require("cors");
 
 // Get routes to the variabel
-const router = require('./src/routes')
+const router = require("./src/routes");
 
-const app = express()
+const app = express();
 
-const port = 5000
+const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 // Add script use cors here ...
+app.use(cors());
 
 // Add endpoint grouping and router
-app.use('/api/v1/', router)
-app.use('/uploads', express.static('uploads'))
+app.use("/api/v1/", router);
+app.use("/uploads", express.static("uploads"));
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
